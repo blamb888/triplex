88a35caebebd3c94b8e4d23619cf0683a6fb4c62
Brandon Lamb <blamb888@gmail.com>
2021-12-14 20:21:07 +0900

    rename variables using Unreal UpperCamelCase convention

--- triplex.cpp
+++ triplex.cpp
@@ -2,20 +2,24 @@
 
 int main()
 {
-    // print variable
+    // Print intro message to terminal
     std::cout << "You are an intrepid adventurer but this door isn't going to budge.";
     std::cout << std::endl;
-    std::cout << "You'll need to enter the right codes to continue...\n";
+    std::cout << "You'll need to enter the right runes to continue...\n";
 
-    const int a = 4;
-    const int b = 3;
-    const int c = 2;
+    // Declare 3 number code
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    // Print sum and product to terminal
+    std::cout << std::endl;
+    std::cout << "+ There are 3 runes in the code" << std::endl;
+    std::cout << "+ The runes add up to: " << CodeSum << std::endl;
+    std::cout << "+ The product of the runes is: " << CodeProduct << std::endl;
 
     return 0;
 }
