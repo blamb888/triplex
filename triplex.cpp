b8ecf5f753b7c9ec04496c03039a3cdcbfecf829
Brandon Lamb <blamb888@gmail.com>
2021-12-14 20:35:52 +0900

    add conditional for guessing correctly

--- triplex.cpp
+++ triplex.cpp
@@ -21,5 +21,20 @@ int main()
     std::cout << "+ The runes add up to: " << CodeSum << std::endl;
     std::cout << "+ The product of the runes is: " << CodeProduct << std::endl;
 
+    int GuessA, GuessB, GuessC;
+
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+    std::cout << "You have etched: " << GuessA << GuessB << GuessC << std::endl;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if ((GuessSum == CodeSum) && (GuessProduct == CodeProduct)) {
+      std::cout << "The door opens and you may proceed..." << std::endl;
+      std::cout << "Well done, wizard." << std::endl;
+    }
+
     return 0;
 }
