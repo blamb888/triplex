abfdbd5164192d1e4b152210757392b77f9cfb7c
Brandon Lamb <blamb888@gmail.com>
2021-12-14 20:07:55 +0900

    learn variable math/const prefixs, namespaces

--- triplex.cpp
+++ triplex.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+
 int main()
 {
+    // print variable
+    std::cout << "You are an intrepid adventurer but this door isn't going to budge.";
+    std::cout << std::endl;
+    std::cout << "You'll need to enter the right codes to continue...\n";
+
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
