49886a8455afa46681175b39c8b8371dd6b877cf
Brandon Lamb <blamb888@gmail.com>
2021-12-15 14:41:17 +0900

    add some flair, update line endings

--- triplex.cpp
+++ triplex.cpp
@@ -3,9 +3,8 @@
 int main()
 {
     // Print intro message to terminal
-    std::cout << "You are an intrepid adventurer but this door isn't going to budge.";
-    std::cout << std::endl;
-    std::cout << "You'll need to enter the right runes to continue...\n";
+    std::cout << "You are an intrepid adventurer but this door isn't going to budge.\n";
+    std::cout << "You'll need to enter the right runes to continue...\n\n";
 
     // Declare 3 number code
     const int CodeA = 4;
@@ -16,30 +15,34 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print sum and product to terminal
-    std::cout << std::endl;
-    std::cout << "+ There are 3 runes in the code" << std::endl;
-    std::cout << "+ The runes add up to: " << CodeSum << std::endl;
-    std::cout << "+ The product of the runes is: " << CodeProduct << std::endl;
+    std::cout << "+ There are 3 runes in the code\n";
+    std::cout << "+ The runes add up to: " << CodeSum;
+    std::cout << "\n+ The product of the runes is: " << CodeProduct << std::endl;
 
+    std::cout << "\n ⚒️  Etch your runes here: ⚒️\n";
+    std::cout << "=================================\n";
     // Store Player gueess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA >> GuessB >> GuessC;
+    std::cout << "⚒️   ", std::cin >> GuessA >> GuessB >> GuessC;
+    std::cout << "=================================\n";
+
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
-    std::cout << "You have etched: " << GuessA << GuessB << GuessC << std::endl;
+    std::cout << "\nYou have etched: " << GuessA << GuessB << GuessC;
 
+    // Check if Player's guess is correct
     if ((GuessSum == CodeSum) && (GuessProduct == CodeProduct))
     {
-      std::cout << "The door opens and you may proceed..." << std::endl;
-      std::cout << "Well done, wizard." << std::endl;
+      std::cout << "\n🗝️ The door opens and you may proceed...🗝️\n";
+      std::cout << "\n🧙‍♂️ Well done, wizard.🧙‍♂️\n";
     }
     else
     {
-      std::cout << "You fool!" << std::endl;
-      std::cout << "You have guessed...poorly" << std::endl;
-      std::cout << "You take 10 poison damage as noxious gas fills the room." << std::endl;
+      std::cout << "\nYou fool!";
+      std::cout << "\nYou have guessed...poorly\n";
+      std::cout << "\n☠️  🤢 You take 10 poison damage as noxious gas fills the room.🤢 ☠️\n";
     }
 
     return 0;
